6d566c2b80f8cc2ccb627ce7f1c7d7d9343525d7
ilya.gavrichenko <gavr.imba@mail.ru>
2018-11-13 16:18:13 +0300

    ability to upload files to the server was added

--- GavrReactDotNetCoreBackend/Controllers/FileUploadController.cs
+++ GavrReactDotNetCoreBackend/Controllers/FileUploadController.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using GavrReactDotNetCoreBackend.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace GavrReactDotNetCoreBackend.Controllers
 {
@@ -16,56 +10,25 @@ namespace GavrReactDotNetCoreBackend.Controllers
     [ApiController]
     public class FileUploadController : Controller
     {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
-        private readonly ApplicationContext _appDbContext;
-        private readonly IHostingEnvironment _appEnvironment;
-
-        public FileUploadController(UserManager<User> userManager, SignInManager<User> signInManager,
-            ApplicationContext appDbContext, IHostingEnvironment appEnvironment)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _appDbContext = appDbContext;
-            _appEnvironment = appEnvironment;
-        }
-
+        //todo: role shall be is admin only
         [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> AddFile(IFormFile file)
         {
-            //// Full path to file in temp location
-            ////var filePath = Path.GetTempFileName();
-            //string filePath = "/image.gavrichenko.ru/" + file.FileName;
-
-            //if (file.Length > 0)
-            //    using (var stream = new FileStream(filePath, FileMode.Create))
-            //        await file.CopyToAsync(stream);
-
-            //// Process uploaded files
-
-            //return Ok(new { count = 1, path = filePath });
-          //  Console.WriteLine(_appEnvironment);
-            return Ok(new { count = 1, path = _appEnvironment.ContentRootPath });
+            var subDomenName = "\\image.gavrichenko.ru";
+            var currentAppDirectory = Directory.GetCurrentDirectory();
+            var pathToStore = Path.Combine(Directory.GetParent(currentAppDirectory).FullName + subDomenName, "flowers", file.FileName);
+            if (file.Length > 0)
+            {
+                using (var stream = new FileStream(pathToStore, FileMode.Create, FileAccess.Write))
+                    await file.CopyToAsync(stream);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+            var url = $"http://image.gavrichenko.ru/flowers/{file.FileName}";
+            return this.Ok(new { url = url });
         }
-        //[AllowAnonymous]
-        //[HttpPost]
-        //public async Task<IActionResult> AddFile(IFormFile uploadedFile)
-        //{
-        //    if (uploadedFile != null)
-        //    {
-        //        string path = "/image.gavrichenko.ru/" + uploadedFile.FileName;
-        //        using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
-        //        {
-        //            await uploadedFile.CopyToAsync(fileStream);
-        //        }
-
-        //        FileModel file = new FileModel {Name = uploadedFile.FileName, Path = path};
-        //        return this.Ok(file);
-
-        //    }
-
-        //    return this.BadRequest("ERROROROR");
-        //}
     }
 }
